5989d689c9b6bc3a9da3008657129362f320ade1
thestoicrohit <thestoicrohit@gmail.com>
2025-12-06 09:52:01 +0530

    Add files via upload

---
+++ Integer-basic.py
@@ -0,0 +1,7 @@
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} kgs of apple")
+print(f"Your class has {num_of_students} students")
