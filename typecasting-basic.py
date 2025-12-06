93a8200f9bc9541df4cbb5e8a7d7ec904e0716ed
thestoicrohit <thestoicrohit@gmail.com>
2025-12-06 09:51:36 +0530

    Add files via upload

---
+++ typecasting-basic.py
@@ -0,0 +1,13 @@
+name = "Rohit"
+age = 25
+gpa = 6.59
+is_student = False
+
+gpa = int(gpa)
+age = float(age)
+is_student = str(is_student)
+
+print(gpa)
+print(age)
+print(type(age))
+print(type(is_student))
